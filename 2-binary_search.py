35a2fd60f1166ac8906c704d04b9b553649d8392
Tamzidul Matin <41488579+tmatin100@users.noreply.github.com>
2022-02-19 08:50:27 -0500

    Add files via upload

---
+++ 2-binary_search.py
@@ -0,0 +1,16 @@
+#Binary Search example
+
+def binary_search(list, target): 
+    first = 0                         # points to the first element in the list
+    last = len(list) - 1              # points to the last element in the list
+
+while first <= last: 
+    midpoint = (first + last)//2     # This is how we calcualte a midpoint
+
+    if list[midpoint] == target:     # if the value of the midpoint is the same as the target, 
+        return midpoint                # we will return the same value 
+    elif list[midpoint] < target     # if the value of the midpoint is less than the target
+        first = midpoint + 1           # we discard everyhing thats lower than the midpoint, and redifine the starting point by setting the to the value of first after the midpoint which is midpoint +1
+    else: 
+       last = midpoint - 1          # if the value is greater than the midpoint, then we discard the values after the midpoint, and redifne last to point to the value prior to midpoint, which is midpoint - 1
+
