48d4fe0695bd389c0d8d0f0b2aeff74bdcbfe769
Tamzidul Matin <41488579+tmatin100@users.noreply.github.com>
2022-02-19 08:26:17 -0500

    Add files via upload

---
+++ 1-linear_search.py
@@ -0,0 +1,22 @@
+def linear_search(list, target):
+    """ Returns the index position of the traget if found, else returns None
+    """
+    for i in range(0,len(list)):
+      if list[i] == target: 
+        return i 
+    return None 
+  
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+def varify(index): 
+  if index is not None: 
+    print("Target found at index:", index)
+  else: 
+    print("Target not found in list")
+
+result = linear_search(numbers, 12)
+
+varify(result)
+
+
+
